123c55b3a7be504fd8980e22a64671c1fa06c38e
HimayunKhan <khanhimayu2@gmail.com>
2023-08-21 11:57:56 +0530

    color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    colors: {
+        primeColor: "#262626",
+        lightText: "#6D6D6D",
+        customGold: "#ddb148",
+        color1:"#F5F5F3",
+      },
     },
   },
   plugins: [],
